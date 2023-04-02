b1a9820b8ddff77ae1e01abfa67943f66d48849b
DanielSHKao <skao@connect.ust.hk>
2023-04-02 03:14:30 -0400

    data update

---
+++ data/data_api.py
@@ -0,0 +1,21 @@
+import os
+import torch
+from torch.utils.data import DataLoader
+import pytorch_lightning as pl
+from utils.utils import *
+from data.data import CommentDataset
+
+class CommentDataModule(pl.LightningDataModule):
+    def __init__(self, data_dir, batch_size, num_workers, columns = ['text','stars'],**kwargs):
+        self.num_workers=num_workers
+        self.batch_size=batch_size
+        self.train_set = CommentDataset(data_dir,columns = columns, split_name = 'train', kwargs)
+        self.val_set = CommentDataset(data_dir,columns = columns, split_name = 'valid', kwargs)
+        self.test_set = CommentDataset(data_dir,columns = columns, split_name = 'test', kwargs)
+    
+    def train_dataloader(self):
+        return DataLoader(self.train_set, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=True)
+    def val_dataloader(self):
+        return DataLoader(self.val_set, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=True)
+    def test_dataloader(self):
+        return DataLoader(self.test_set, batch_size=self.batch_size, num_workers=self.num_workers, pin_memory=True)
---
+++ utils/utils.py
@@ -0,0 +1,123 @@
+import pandas as pd
+
+def load_data(split_name='train', columns=['text', 'stars'], folder='./../data'):
+    '''
+        "split_name" may be set as 'train', 'valid' or 'test' to load the corresponding dataset.
+        
+        You may also specify the column names to load any columns in the .csv data file.
+        Among many, "text" can be used as model input, and "stars" column is the labels (sentiment). 
+        If you like, you are free to use columns other than "text" for prediction.
+    '''
+    try:
+        print(f"select [{', '.join(columns)}] columns from the {split_name} split")
+        df = pd.read_csv(f'{folder}/{split_name}.csv')
+        df = df.loc[:,columns]
+        print("Success")
+        return df
+    except:
+        print(f"Failed loading specified columns... Returning all columns from the {split_name} split")
+        df = pd.read_csv(f'{folder}/{split_name}.csv')
+        return df
+
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+stopwords = set(stopwords.words('english'))
+ps = PorterStemmer()
+
+def lower(s):
+    """
+    :param s: a string.
+    return a string with lower characters
+    Note that we allow the input to be nested string of a list.
+    e.g.
+    Input: 'Text mining is to identify useful information.'
+    Output: 'text mining is to identify useful information.'
+    """
+    if isinstance(s, list):
+        return [lower(t) for t in s]
+    if isinstance(s, str):
+        return s.lower()
+    else:
+        raise NotImplementedError("unknown datatype")
+
+
+def tokenize(text):
+    """
+    :param text: a doc with multiple sentences, type: str
+    return a word list, type: list
+    e.g.
+    Input: 'Text mining is to identify useful information.'
+    Output: ['Text', 'mining', 'is', 'to', 'identify', 'useful', 'information', '.']
+    """
+    return nltk.word_tokenize(text)
+
+
+def stem(tokens):
+    """
+    :param tokens: a list of tokens, type: list
+    return a list of stemmed words, type: list
+    e.g.
+    Input: ['Text', 'mining', 'is', 'to', 'identify', 'useful', 'information', '.']
+    Output: ['text', 'mine', 'is', 'to', 'identifi', 'use', 'inform', '.']
+    """
+    ### equivalent code
+    # results = list()
+    # for token in tokens:
+    #     results.append(ps.stem(token))
+    # return results
+
+    return [ps.stem(token) for token in tokens]
+
+def n_gram(tokens, n=1):
+    """
+    :param tokens: a list of tokens, type: list
+    :param n: the corresponding n-gram, type: int
+    return a list of n-gram tokens, type: list
+    e.g.
+    Input: ['text', 'mine', 'is', 'to', 'identifi', 'use', 'inform', '.'], 2
+    Output: ['text mine', 'mine is', 'is to', 'to identifi', 'identifi use', 'use inform', 'inform .']
+    """
+    if n == 1:
+        return tokens
+    else:
+        results = list()
+        for i in range(len(tokens)-n+1):
+            # tokens[i:i+n] will return a sublist from i th to i+n th (i+n th is not included)
+            results.append(" ".join(tokens[i:i+n]))
+        return results
+
+def filter_stopwords(tokens):
+    """
+    :param tokens: a list of tokens, type: list
+    return a list of filtered tokens, type: list
+    e.g.
+    Input: ['text', 'mine', 'is', 'to', 'identifi', 'use', 'inform', '.']
+    Output: ['text', 'mine', 'identifi', 'use', 'inform', '.']
+    """
+    ### equivalent code
+    # results = list()
+    # for token in tokens:
+    #     if token not in stopwords and not token.isnumeric():
+    #         results.append(token)
+    # return results
+
+    return [token for token in tokens if token not in stopwords and not token.isnumeric()]
+
+import numpy as np
+
+def get_onehot_vector(feats, feats_dict):
+    """
+    :param data: a list of features, type: list
+    :param feats_dict: a dict from features to indices, type: dict
+    return a feature vector,
+    """
+    # initialize the vector as all zeros
+    vector = np.zeros(len(feats_dict), dtype=np.float)
+    for f in feats:
+        # get the feature index, return -1 if the feature is not existed
+        f_idx = feats_dict.get(f, -1)
+        if f_idx != -1:
+            # set the corresponding element as 1
+            vector[f_idx] = 1
+    return vector
