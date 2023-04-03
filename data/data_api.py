607ee64c8272fd47f320ebbe4ef30d5a3b8e70f8
Daniel Kao <80144650+DanielSHKao@users.noreply.github.com>
2023-04-03 11:12:30 +0800

    Update data_api.py

--- data/data_api.py
+++ data/data_api.py
@@ -8,16 +8,16 @@ from data.data import CommentDataset
 import random
 
 class CommentDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir, batch_size, num_workers):
+    def __init__(self, data_dir, columns, batch_size, num_workers, **kwargs):
         super().__init__()
         self.data_dir = data_dir
         self.num_workers = num_workers
         self.batch_size = batch_size
-
+        self.columns = columns
     def setup(self, stage: str):
-        self.train_set = CommentDataset(self.data_dir, columns=['text','stars'], split_name='train')
-        self.val_set = CommentDataset(self.data_dir, columns=['text','stars'], split_name='valid')
-        self.test_set = CommentDataset(self.data_dir, columns=['all'], split_name='test')
+        self.train_set = CommentDataset(self.data_dir, columns=self.columns, split_name='train')
+        self.val_set = CommentDataset(self.data_dir, columns=self.columns, split_name='valid')
+        self.test_set = CommentDataset(self.data_dir, columns=self.columns, split_name='test')
     
     def train_dataloader(self):
         return DataLoader(self.train_set, batch_size=self.batch_size, num_workers=self.num_workers, shuffle=True, pin_memory=True)
